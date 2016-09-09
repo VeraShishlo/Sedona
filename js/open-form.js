e43d00e3ac1fd6f5700b507c45b8476a8c11d7ec
VeraTerengina <shishlonik@mail.ru>
2016-09-09 11:06:05 +0300

    Sedona
    Sedona project

---
+++ js/open-form.js
@@ -0,0 +1,6 @@
+var challenge = document.querySelector(".search-form-challenge");
+			var form = document.querySelector(".search-form");
+			var map = document.querySelector(".map");
+			challenge.addEventListener("click", function(event) {
+				form.classList.toggle("search-form-show")
+			});
